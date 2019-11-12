cb8edcc3ea0fb97318322c9dc2a70b8f1ca5ede0
Marchelo Biondi <22040@columbushs.com>
2019-11-12 14:44:55 -0500

    rover

--- mars-rover-v2/javascripts/app.js
+++ mars-rover-v2/javascripts/app.js
@@ -1,13 +1,48 @@
-// Rover Object Goes Here
+// Rover Object Goes Here{
 // ======================
-
+let  rover =  {
+  direction: 'N',
+}
 // ======================
 function turnLeft(rover){
   console.log("turnLeft was called!");
+
+if(rover.direction == 'N'){
+  rover.direction = 'W'
+}
+else if(rover.direction == 'W'){
+  rover.direction = 'S'
+}
+
+else if (rover.direction == 'S'){
+  rover.direction = 'E'
+}
+else if(rover.direction == 'E'){
+  rover.direction = 'N'
+}
+
 }
 
 function turnRight(rover){
   console.log("turnRight was called!");
+
+if(rover.direction == 'N'){
+  rover.direction = 'E'
+}
+
+else if(rover.direction == 'W'){
+  rover.direction = 'S'
+}
+
+else if(rover.direction == 'S'){  
+rover.direction = 'W'
+}
+
+else if(rover.direction == 'W'){
+  rover.direction = 'N'
+}
+
+
 }
 
 function moveForward(rover){
